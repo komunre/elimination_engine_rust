09fc15e6ebccfe9edd9cf5c4623ed0a365c695b3
komunre <koliziy5@gmail.com>
2023-12-10 18:48:43 +0700

    Triangle rendering fixed

--- komrend/src/lib.rs
+++ komrend/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate glfw;
 extern crate gl;
 
+use std::ffi::{CString, c_char};
+
 use glfw::{Action, Context, Key, fail_on_errors, PWindow, Glfw, InitError, WindowEvent, GlfwReceiver, ffi::{glfwTerminate, glfwSwapBuffers, glfwInit, glfwWindowHint, GLFWwindow, glfwCreateWindow, glfwMakeContextCurrent, glfwSetFramebufferSizeCallback, glfwGetProcAddress, glfwPollEvents, glfwGetFramebufferSize, glfwWindowShouldClose}};
 
 pub enum ShaderType {
@@ -59,20 +61,26 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
 pub fn load_shader(path: &str, ty: ShaderType) -> Result<u32, std::io::Error> {
     println!("Loading shader of path: {}", path);
     let data = std::fs::read(path)?;
-    let mut d2 = Vec::<*const i8>::new();
-    let mut d3 = Vec::<i8>::new();
-    for d in data {
-        d3.push(d as i8);
-    }
-    d2.push(d3.as_ptr());
+    let dstr = unsafe {CString::from_vec_unchecked(data) };
+    let dcstr = dstr.as_c_str();
     unsafe {
     let sh = gl::CreateShader(match ty {
          ShaderType::VERTEX => gl::VERTEX_SHADER,
          ShaderType::FRAGMENT => gl::FRAGMENT_SHADER,
          ShaderType::GEOMETRY => gl::GEOMETRY_SHADER,
     });
-    gl::ShaderSource(sh, 1, d2.as_ptr(), std::ptr::null());
+    gl::ShaderSource(sh, 1, &dcstr.as_ptr(), std::ptr::null());
     gl::CompileShader(sh);
+    let mut success: i32 = 0;
+    gl::GetShaderiv(sh, gl::COMPILE_STATUS, &mut success);
+    if success != 1 {
+        let mut buffer = Vec::<u8>::with_capacity(512);
+        buffer.extend([b' '].iter().cycle().take(512));
+        let infolog: CString = CString::from_vec_unchecked(buffer);
+        gl::GetShaderInfoLog(sh, 512, 0 as *mut i32, infolog.as_ptr() as *mut c_char);
+        println!("Error compiling shader: {}", infolog.to_str().unwrap());
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, infolog.to_str().unwrap()));
+    }
     Ok(sh)
     }
 }
@@ -84,6 +92,15 @@ pub fn create_shader_program(v: u32, f: u32) -> u32 {
         gl::AttachShader(program, v);
         gl::AttachShader(program, f);
         gl::LinkProgram(program);
+        let mut success: i32 = 0;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut success);
+        if success != 1 {
+            let mut buffer = Vec::<u8>::with_capacity(512);
+            buffer.extend([b' '].iter().cycle().take(512));
+            let infolog = CString::from_vec_unchecked(buffer);
+            gl::GetProgramInfoLog(program, 512, 0 as *mut i32, infolog.as_ptr() as *mut c_char);
+            println!("Error linking program: {}", infolog.to_str().unwrap());
+        }
         program
     }
 }
@@ -114,7 +131,7 @@ impl Komrend {
     pub fn do_triangle(&self) -> (u32, u32, u32) {
         let mut vbo: u32 = 0;
         let mut vao: u32 = 0;
-        const VERTS: [f64; 9] = [
+        const VERTS: [f32; 9] = [
             -0.5, -0.5, 0.0,
             0.5, -0.5, 0.0,
             0.0, 0.5, 0.0,
@@ -124,10 +141,11 @@ impl Komrend {
             gl::GenVertexArrays(1, &mut vao);
             gl::BindVertexArray(vao);
             gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::BufferData(gl::ARRAY_BUFFER, (std::mem::size_of::<f64>() * 9) as isize, VERTS.as_ptr() as *const _, gl::STATIC_DRAW);
+            println!("Size: {}", (std::mem::size_of::<f32>() * 9) as isize);
+            gl::BufferData(gl::ARRAY_BUFFER, (std::mem::size_of::<f32>() * 9) as isize, VERTS.as_ptr() as *const _, gl::STATIC_DRAW);
             let program = create_shader_program(load_shader("resources/shaders/default.vs", ShaderType::VERTEX).unwrap(), load_shader("resources/shaders/default.fs", ShaderType::FRAGMENT).unwrap());
             gl::UseProgram(program);
-            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * std::mem::size_of::<f64>() as i32, 0 as *const _);
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * std::mem::size_of::<f32>() as i32, 0 as *const _);
             gl::EnableVertexAttribArray(0);
             (vao, vbo, program)
         }
